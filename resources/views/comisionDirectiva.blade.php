deb657ec2e7710dfdd2ef20d648dacef08e3a6e7
DucaMartin <martinducaangriman@gmail.com>
2019-08-14 11:53:39 -0300

    CD actualizado

--- resources/views/comisionDirectiva.blade.php
+++ resources/views/comisionDirectiva.blade.php
@@ -4,16 +4,16 @@
 <div class="cd" style="background-color: white">
 
 
-<h1>COMISION DIRECTIVA DE MISIONES</h1>
+<h1 class="text-center">COMISION DIRECTIVA PROVINCIAL</h1>
 
 
   <table class="table table-bordered table-dark">
 <!-- CABEZA -->
   <thead>
     <tr>
-      <th scope="col">1</th>
-      <th scope="col">2</th>
-      <th scope="col">3</th>
+      <th scope="col">Secretaría</th>
+      <th scope="col">Secretario</th>
+      <th scope="col">Org. de Base</th>
 
 
     </tr>
@@ -21,77 +21,35 @@
   <!-- FILAS -->
   <tbody>
     <tr>
-      <th scope="row">SECRETARIO GENERAL</th>
+      <th scope="row">Secretario Gral</th>
       <td>Jorge Gabriel Duarte</td>
       <td>ATE</td>
 
 
     </tr>
     <tr>
-      <th scope="row">SECRETARIA</th>
-      <td>NOMBRE DIRIGENTE</td>
+      <th scope="row">Secretaria Gral Adj</th>
+      <td>Miriam Elizabet López</td>
       <td>AGRUPACION</td>
 
     </tr>
     <tr>
-      <th scope="row">SECRETARIA</th>
-      <td >NOMBRE DIRIGENTE</td>
+      <th scope="row">Secretario Gral Adj</th>
+      <td>Leandro Javier Sánchez</td>
       <td>AGRUPACION</td>
 
     </tr>
-
-
-
-    <tr>
-      <th scope="row">SECRETARIA</th>
-      <td >NOMBRE DIRIGENTE</td>
-      <td>AGRUPACION</td>
-
-    </tr>
-    <tr>
-      <th scope="row">SECRETARIA</th>
-      <td >NOMBRE DIRIGENTE</td>
-      <td>AGRUPACION</td>
-
-    </tr>
-    <tr>
-      <th scope="row">SECRETARIA</th>
-      <td >NOMBRE DIRIGENTE</td>
-      <td>AGRUPACION</td>
-
-    </tr>
-    <tr>
-      <th scope="row">SECRETARIA</th>
-      <td >NOMBRE DIRIGENTE</td>
-      <td>AGRUPACION</td>
-
-    </tr>
-    <tr>
-      <th scope="row">SECRETARIA</th>
-      <td >NOMBRE DIRIGENTE</td>
-      <td>AGRUPACION</td>
-
-    </tr>
-    <tr>
-      <th scope="row">SECRETARIA</th>
-      <td >NOMBRE DIRIGENTE</td>
-      <td>AGRUPACION</td>
-
-    </tr>
-
-
-  </tbody>
 </table>
 
 
-<h2>VOCALES</h2>
+<h2 class="text-center">Comisión Ejecutiva Local Posadas</h2>
 <table class="table table-bordered table-dark">
 <!-- CABEZA -->
 <thead>
   <tr>
-    <th scope="col">1</th>
-    <th scope="col">2</th>
-    <th scope="col">3</th>
+    <th scope="col">Secretaría</th>
+    <th scope="col">Secretario</th>
+    <th scope="col">Org. de Base</th>
 
 
   </tr>
@@ -99,28 +57,294 @@
 <!-- FILAS -->
 <tbody>
   <tr>
-    <th scope="row">SECRETARIA</th>
-    <td>NOMBRE DIRIGENTE</td>
+    <th scope="row">Secretario Gral</th>
+    <td>Marcelo Fabían Grasiadei</td>
     <td>AGRUPACION</td>
 
 
   </tr>
   <tr>
-    <th scope="row">SECRETARIA</th>
-    <td>NOMBRE DIRIGENTE</td>
+    <th scope="row">Secretario Gral Adj</th>
+    <td>Carlos Wilfrido Candia</td>
     <td>AGRUPACION</td>
 
   </tr>
-  <tr>
-    <th scope="row">SECRETARIA</th>
-    <td >NOMBRE DIRIGENTE</td>
-    <td>AGRUPACION</td>
 
-  </tr>
 </tbody>
 </table>
 
+
+
+<h2 class="text-center">Comisión Ejecutiva Local Oberá </h2>
+<table class="table table-bordered table-dark">
+<!-- CABEZA -->
+<thead>
+  <tr>
+    <th scope="col">Secretaría</th>
+    <th scope="col">Secretario</th>
+    <th scope="col">Org. de Base</th>
+
+
+  </tr>
+</thead>
+<!-- FILAS -->
+<tbody>
+  <tr>
+    <th scope="row">Secretaria Gral</th>
+    <td>Liliana Mabel Muller</td>
+    <td>AGRUPACION</td>
+
+
+  </tr>
+  <tr>
+    <th scope="row">Secretario Gral Adj</th>
+    <td>Carlos Antonio Ayala</td>
+    <td>AGRUPACION</td>
+
+  </tr>
+
+</tbody>
+</table>
+
+
+<h2 class="text-center">Comisión Ejecutiva Local Puerto Iguazú </h2>
+<table class="table table-bordered table-dark">
+<!-- CABEZA -->
+<thead>
+  <tr>
+    <th scope="col">Secretaría</th>
+    <th scope="col">Secretario</th>
+    <th scope="col">Org. de Base</th>
+
+
+  </tr>
+</thead>
+<!-- FILAS -->
+<tbody>
+  <tr>
+    <th scope="row">Secretario Gral</th>
+    <td>Edgardo Landa</td>
+    <td>AGRUPACION</td>
+
+
+  </tr>
+  <tr>
+    <th scope="row">Secretario Gral Adj</th>
+    <td>Lucas Loza</td>
+    <td>AGRUPACION</td>
+
+  </tr>
+
+</tbody>
+</table>
+
+
+<h2 class="text-center">Comisión Ejecutivo Local L.N. Alem </h2>
+<table class="table table-bordered table-dark">
+<!-- CABEZA -->
+<thead>
+  <tr>
+    <th scope="col">Secretaría</th>
+    <th scope="col">Secretario</th>
+    <th scope="col">Org. de Base</th>
+
+
+  </tr>
+</thead>
+<!-- FILAS -->
+<tbody>
+  <tr>
+    <th scope="row">Secretario Gral</th>
+    <td>Juan Andres Marquez</td>
+    <td>AGRUPACION</td>
+
+
+  </tr>
+  <tr>
+    <th scope="row">Secretaria Gral Adj</th>
+    <td>Paulina Leocadia Marczeski</td>
+    <td>AGRUPACION</td>
+
+  </tr>
+
+</tbody>
+</table>
+
+
+
+<h2 class="text-center">Comisión Ejecutivo Local L.N. Alem </h2>
+<table class="table table-bordered table-dark">
+<!-- CABEZA -->
+<thead>
+  <tr>
+    <th scope="col">Secretaría</th>
+    <th scope="col">Secretario</th>
+    <th scope="col">Org. de Base</th>
+
+
+  </tr>
+</thead>
+<!-- FILAS -->
+<tbody>
+  <tr>
+    <th scope="row">Secretario Gral</th>
+    <td>Juan Andres Marquez</td>
+    <td>AGRUPACION</td>
+
+
+  </tr>
+  <tr>
+    <th scope="row">Secretaria Gral Adj</th>
+    <td>Paulina Leocadia Marczeski</td>
+    <td>AGRUPACION</td>
+
+  </tr>
+
+</tbody>
+</table>
+
+<h2 class="text-center">Comisión Ejecutiva Local Apóstoles </h2>
+<table class="table table-bordered table-dark">
+<!-- CABEZA -->
+<thead>
+  <tr>
+    <th scope="col">Secretaría</th>
+    <th scope="col">Secretario</th>
+    <th scope="col">Org. de Base</th>
+
+
+  </tr>
+</thead>
+<!-- FILAS -->
+<tbody>
+  <tr>
+    <th scope="row">Secretario Gral</th>
+    <td>Horacio Luis Dumañnski</td>
+    <td>AGRUPACION</td>
+
+
+  </tr>
+  <tr>
+    <th scope="row">Secretaria Gral Adj</th>
+    <td>Lucia Mabel Tarnouski</td>
+    <td>AGRUPACION</td>
+
+  </tr>
+
+</tbody>
+</table>
+
+
+<h2 class="text-center">Comisión Ejecutiva Local Puerto Rico </h2>
+<table class="table table-bordered table-dark">
+<!-- CABEZA -->
+<thead>
+  <tr>
+    <th scope="col">Secretaría</th>
+    <th scope="col">Secretario</th>
+    <th scope="col">Org. de Base</th>
+
+
+  </tr>
+</thead>
+<!-- FILAS -->
+<tbody>
+  <tr>
+    <th scope="row">Secretaria Gral</th>
+    <td>Juana Rosa Paniagua</td>
+    <td>AGRUPACION</td>
+
+
+  </tr>
+  <tr>
+    <th scope="row">Secretaria Gral Adj</th>
+    <td>Patricia Isabel Martínez </td>
+    <td>AGRUPACION</td>
+
+  </tr>
+
+</tbody>
+</table>
+
+
+<h2 class="text-center">Comisión Ejecutiva Local Wanda </h2>
+<table class="table table-bordered table-dark">
+<!-- CABEZA -->
+<thead>
+  <tr>
+    <th scope="col">Secretaría</th>
+    <th scope="col">Secretario</th>
+    <th scope="col">Org. de Base</th>
+
+
+  </tr>
+</thead>
+<!-- FILAS -->
+<tbody>
+  <tr>
+    <th scope="row">Secretaria Gral</th>
+    <td>Iris Magdalena Morinigo</td>
+    <td>AGRUPACION</td>
+
+
+  </tr>
+  <tr>
+    <th scope="row">Secretaria Gral Adj</th>
+    <td>Rosa Iris Velázquez  </td>
+    <td>AGRUPACION</td>
+
+  </tr>
+
+</tbody>
+</table>
+
+<h2 class="text-center">Comisión Ejecutiva Local Eldorado </h2>
+<table class="table table-bordered table-dark">
+<!-- CABEZA -->
+<thead>
+  <tr>
+    <th scope="col">Secretaría</th>
+    <th scope="col">Secretario</th>
+    <th scope="col">Org. de Base</th>
+
+
+  </tr>
+</thead>
+<!-- FILAS -->
+<tbody>
+  <tr>
+    <th scope="row">Secretario Gral</th>
+    <td>Alejandro Carballo</td>
+    <td>AGRUPACION</td>
+
+
+  </tr>
+  <tr>
+    <th scope="row">Secretario Gral Adj</th>
+    <td>Ramón Ariste </td>
+    <td>AGRUPACION</td>
+
+  </tr>
+
+</tbody>
+</table>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </div>
-
-
 @endsection
