5d707040e7d7d7ebbad72afd62a73a7332d84ca5
ducamartin <martinducaangriman@gmail.com>
2019-09-06 18:01:45 -0300

    Trabajo al 6/9

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -12,14 +12,43 @@
 <div class="containerC">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-          
-        
-          
-          </div>
-      </div>  
-     </div>
+        <div class="card mb-3" style="padding: 20px">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3543.2042313923225!2d-55.90398768494526!3d-27.369336482933093!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9457be3766252e3d%3A0x78899dbc1167b08f!2sAv.%20Corrientes%202150%2C%20N3300NAK%20Posadas%2C%20Misiones!5e0!3m2!1ses!2sar!4v1567801800757!5m2!1ses!2sar" width="200" height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Sede nacional de la Central de Trabajadores de la Argentina Autónoma en Misiones:</h5>
+        <p class="card-text">Av. Corrientes 2150, Posadas. C.P 3300. República Argentina</p>
+       <p class="card-text">Teléfono: <a href="tel:+03764428901"> 0376 442-8901</p></a>
+      </div>
+    </div>
   </div>
+
+
+  <div class="card mb-8 text-center">
+  <div  class="card text-center" >
+
+    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCtaMisiones%2F&tabs=timeline&width=800&height=500&small_header=true&adapt_container_width=false&hide_cover=false&show_facepile=true&appId" width="700" height="350" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+    </div>
+
+  <div class="card-body">
+    <h5 class="card-title">Seguinos en nuestro Facebook</h5>
+  </div>
+</div>
+
+
+<div class="card text-center">
+  <div class="card-body">
+    <h5 class="card-title">Contacto vía mail</h5>
+    <p class="card-text">mail@mail.com</p>
+  </div>
+</div>
+        </div>
+      </div>
+    </div>
+</div>
 </body>
 </html>
 
